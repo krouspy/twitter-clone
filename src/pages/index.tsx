ff8f775f1f10a1349035b534489cc62dde0a21b8
krouspy <knjlau1@gmail.com>
2021-12-28 14:14:30 +0100

    add path aliases
    - components
    - routers
    - trpc
    - utils

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import { trpc } from '../utils/trpc';
+import { trpc } from '@/utils';
 
 const Home: NextPage = () => {
   const hello = trpc.useQuery(['hello', { text: 'client' }]);
