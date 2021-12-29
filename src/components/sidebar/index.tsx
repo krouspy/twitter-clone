08a990e7c70b80514499d6676995df3b98bb599b
krouspy <knjlau1@gmail.com>
2021-12-29 21:44:10 +0100

    wrap button inside link

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link';
 import { Box, Stack, Button } from '@chakra-ui/react';
 import { TweetButton } from '@/components';
 import buttonsData from './data';
@@ -6,18 +7,20 @@ import buttonsData from './data';
 export const Sidebar: React.FC = () => {
   return (
     <Stack direction="column" spacing="5" mx="7%">
-      {buttonsData.map(({ id, label, Icon }, index) => {
+      {buttonsData.map(({ id, label, Icon, href }, index) => {
         const isFirstItem = index === 0;
         return (
           <Box key={id}>
-            <Button
-              variant="ghost"
-              leftIcon={<Icon fontSize={isFirstItem ? '35' : '25'} />}
-              fontSize={{ base: 'lg', xl: 'xl' }}
-              fontWeight="normal"
-            >
-              {label}
-            </Button>
+            <Link href={href} passHref>
+              <Button
+                variant="ghost"
+                leftIcon={<Icon fontSize={isFirstItem ? '35' : '25'} />}
+                fontSize={{ base: 'lg', xl: 'xl' }}
+                fontWeight="normal"
+              >
+                {label}
+              </Button>
+            </Link>
           </Box>
         );
       })}
